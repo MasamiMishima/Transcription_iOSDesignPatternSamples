12bf10d544aa41c13e00e96eeb839fd8fc6a3e0b
MasamiMishima <mishima0304@gmail.com>
2018-12-03 23:56:49 +0900

    add UserRepositoryView protocol

--- Transcription_iOSDesignPatternSamples/Sources/UI/UserRepository/UserRepositoryViewPresenter.swift
+++ Transcription_iOSDesignPatternSamples/Sources/UI/UserRepository/UserRepositoryViewPresenter.swift
@@ -9,4 +9,16 @@
 import Foundation
 import GithubKit
 
+protocol UserRepositoryPresenter: class {
+    init(user: User)
+    weak var view: UserRepositoryView? { get set }
+    var title: String { get }
+    var isFetchingRepositories: Bool { get }
+    var numberOfRepositories: Int { get }
+    func repository(at index: Int) -> Repository
+    func showRepository(at index: Int)
+    func showLoadingView(on view: UIView)
+    func setIsReachedBottom(_ isReachedBottom: Bool)
+    func fetchRepositories()
+}
 
